cbfe0611774af33d35b04f457d6a46df877ef925
Simeon Raykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-09-24 22:51:09 +0300

    Add files via upload

---
+++ JS-Fundamentals/JS Expressions, Conditional Statements and Loops-exercises/Multiplication Table.js
@@ -0,0 +1,29 @@
+function multiplicationTable(num) {
+
+    let text = `<table border="1">`;
+    text += `<tr><th>x</th>`;
+
+    for (let index = 1; index <= num; index++) {
+
+        text += `<th>${index}</th>`;
+
+    }
+    text += `</tr>`;
+
+    for (let i = 1; i <= num; i++) {
+
+        text += `<tr><th>${i}</th>`;
+
+        for (let j = 0; j <= num; j++) {
+
+            text+=`<td>${i+j}</td>`;
+
+        }
+        text += `</tr>`;
+
+    }
+
+    text += `</table>`;
+    console.log(text);
+}
+multiplicationTable(2);
